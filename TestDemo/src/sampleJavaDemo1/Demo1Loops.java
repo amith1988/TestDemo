4ae0db83b948527c5f2c7a754a2977ebf41c6403
amithada123 <amitsingh.hada@gmail.com>
2018-10-31 15:47:10 +0530

    changes

---
+++ TestDemo/src/sampleJavaDemo1/Demo1Loops.java
@@ -0,0 +1,15 @@
+package sampleJavaDemo1;
+
+public class Demo1Loops {
+	public static void main(String args[]){
+		
+		int a=2;
+		System.out.println(a);
+		
+		for ( a=4; a<10;a++){
+			System.out.println(a);
+		}
+		
+	}
+
+}
